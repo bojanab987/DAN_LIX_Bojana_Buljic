7638f5317383a3949dbbf8c6b2239a760888cb0d
bojanabb <bojana.buljic@gmail.com>
2020-09-02 16:29:54 +0200

    Add model and folder structure

---
+++ DAN_LIX_Bojana_Buljic/Zadatak_1/Model/CardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Model
+{
+    public class CardModel
+    {
+        public int Id { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
