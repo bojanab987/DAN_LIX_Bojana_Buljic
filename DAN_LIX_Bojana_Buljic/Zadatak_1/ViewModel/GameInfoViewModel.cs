6eee20f4ddb0c2fbdc1969283cbf65ccc09301e4
bojanabb <bojana.buljic@gmail.com>
2020-09-02 23:38:14 +0200

    GameInfo, SlideCollection and Timer ViewModel class logic implemented

--- DAN_LIX_Bojana_Buljic/Zadatak_1/ViewModel/GameInfoViewModel.cs
+++ DAN_LIX_Bojana_Buljic/Zadatak_1/ViewModel/GameInfoViewModel.cs
@@ -3,11 +3,128 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Zadatak_1.ViewModel
 {
     public class GameInfoViewModel:ViewModelBase
     {
+        private const int _maxAttempts = 10;
+        private const int _pointAward = 75;
+        private const int _pointDeduction = 15;
+        private bool _gameLost;
+        private bool _gameWon;
+        /// <summary>
+        /// No of match attempts
+        /// </summary>
+        private int _matchAttempts;
+        public int MatchAttempts
+        {
+            get
+            {
+                return _matchAttempts;
+            }
+            private set
+            {
+                _matchAttempts = value;
+                OnPropertyChanged("MatchAttempts");
+            }
+        }
 
+        /// <summary>
+        /// Players score
+        /// </summary>
+        private int _score;
+        public int Score
+        {
+            get
+            {
+                return _score;
+            }
+            private set
+            {
+                _score = value;
+                OnPropertyChanged("Score");
+            }
+        }
+
+        /// <summary>
+        /// Setting the viisibility for information when game is lost
+        /// </summary>        
+        public Visibility LostMessage
+        {
+            get
+            {
+                if (_gameLost)
+                    return Visibility.Visible;
+
+                return Visibility.Hidden;
+            }
+        }
+
+        /// <summary>
+        /// Setting the viisibility for information when game is won
+        /// </summary>        
+        public Visibility WinMessage
+        {
+            get
+            {
+                if (_gameWon)
+                    return Visibility.Visible;
+
+                return Visibility.Hidden;
+            }
+        }
+
+        /// <summary>
+        /// Decision on game status (if its won or lost)
+        /// </summary>
+        /// <param name="win">boolean value for game status</param>
+        public void GameStatus(bool win)
+        {
+            //if lost show message for lossing
+            if (!win)
+            {
+                _gameLost = true;
+                OnPropertyChanged("LostMessage");
+            }
+            //if lost show message about winning
+            if (win)
+            {
+                _gameWon = true;
+                OnPropertyChanged("WinMessage");
+            }
+        }
+
+        /// <summary>
+        /// Clearing the Game Board
+        /// </summary>
+        public void ClearInfo()
+        {
+            Score = 0;
+            MatchAttempts = _maxAttempts;
+            _gameLost = false;
+            _gameWon = false;
+            OnPropertyChanged("LostMessage");
+            OnPropertyChanged("WinMessage");
+        }
+
+        /// <summary>
+        /// Awarding points for wins
+        /// </summary>
+        public void Award()
+        {
+            Score += _pointAward;            
+        }
+
+        /// <summary>
+        /// Penalty points for looses
+        /// </summary>
+        public void Penalize()
+        {
+            Score -= _pointDeduction;
+            MatchAttempts--;
+            
+        }
     }
 }
--- DAN_LIX_Bojana_Buljic/Zadatak_1/ViewModel/TimerViewModel.cs
+++ DAN_LIX_Bojana_Buljic/Zadatak_1/ViewModel/TimerViewModel.cs
@@ -1,12 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace Zadatak_1.ViewModel
 {
+    /// <summary>
+    /// Class for handling timer in game
+    /// </summary>
     public class TimerViewModel:ViewModelBase
     {
+        //Timer to keep track of length of playing
+        private DispatcherTimer _playedTimer;        
+        private const int _playSeconds = 1;
+
+        #region Constructor
+        public TimerViewModel(TimeSpan time)
+        {
+            _playedTimer = new DispatcherTimer();
+            _playedTimer.Interval = time;
+            _playedTimer.Tick += PlayedTimer_Tick;
+            _timePlayed = new TimeSpan();
+        }
+        #endregion        
+
+        /// <summary>
+        /// Time spent in one game
+        /// </summary>
+        private TimeSpan _timePlayed;
+        public TimeSpan Time
+        {
+            get
+            {
+                return _timePlayed;
+            }
+            set
+            {
+                _timePlayed = value;
+                OnPropertyChanged("Time");
+            }
+        }       
+
+        /// <summary>
+        /// Starting timer method
+        /// </summary>
+        public void Start()
+        {
+            _playedTimer.Start();
+        }
+
+        /// <summary>
+        /// Stoping timer method
+        /// </summary>
+        public void Stop()
+        {
+            _playedTimer.Stop();
+        }
+
+        /// <summary>
+        /// Event of ticking timer added to time property
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PlayedTimer_Tick(object sender, EventArgs e)
+        {
+            Time = _timePlayed.Add(new TimeSpan(0, 0, 1));
+        }
     }
 }
