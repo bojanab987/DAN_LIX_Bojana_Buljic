c2e31d3eb2d7b3344d7b13c62d2c393220a1d6ff
bojanabb <bojana.buljic@gmail.com>
2020-09-02 18:13:59 +0200

    GameMenu window and all ViewModels added

--- DAN_LIX_Bojana_Buljic/Zadatak_1/ViewModel/GameViewModel.cs
+++ DAN_LIX_Bojana_Buljic/Zadatak_1/ViewModel/GameViewModel.cs
@@ -17,10 +17,37 @@ namespace Zadatak_1.ViewModel
     }
     public class GameViewModel:ViewModelBase
     {
+        #region Constructor
         public GameViewModel(SlideCategories category)
         {
-            //Category = category;
-            //SetupGame(category);
+            Category = category;
+            SetupGame(category);
         }
+        #endregion
+
+        #region Properties
+        //Collection of slides we are playing with
+        public SlideCollectionViewModel Slides { get; private set; }
+        //Game information scores, attempts etc
+        public GameInfoViewModel GameInfo { get; private set; }
+        //Game timer for elapsed time
+        public TimerViewModel Timer { get; private set; }
+        //Category we are playing in
+        public SlideCategories Category { get; private set; }
+        #endregion
+
+        //Initialize game essentials
+        private void SetupGame(SlideCategories category)
+        {
+
+           
+            //Slides have been updated
+            OnPropertyChanged("Slides");
+            OnPropertyChanged("Timer");
+            OnPropertyChanged("GameInfo");
+        }
+
+        
+
     }
 }
--- DAN_LIX_Bojana_Buljic/Zadatak_1/ViewModel/StartMenuViewModel.cs
+++ DAN_LIX_Bojana_Buljic/Zadatak_1/ViewModel/StartMenuViewModel.cs
@@ -1,50 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Zadatak_1.Commands;
 
 namespace Zadatak_1.ViewModel
 {
     public class StartMenuViewModel
     {
         private MainWindow _mainWindow;
+        #region Constructor
         public StartMenuViewModel(MainWindow main)
         {
             _mainWindow = main;            
         }
+        #endregion
 
+        #region Method to Start New Game
+        /// <summary>
+        /// Opens Game view and starts new game
+        /// </summary>
+        /// <param name="categoryIndex"></param>
         public void StartNewGame(int categoryIndex)
         {
             var category = (SlideCategories)categoryIndex;
             GameViewModel newGame = new GameViewModel(category);
             _mainWindow.DataContext = newGame;
         }
+        #endregion
 
-        //private ICommand play;
-        //public ICommand Play
-        //{
-        //    get
-        //    {
-        //        if (play == null)
-        //        {
-        //            play = new RelayCommand(param => PlayExecute(), param => CanPlayExecute());
-        //        }
-        //        return play;
-        //    }
-        //}
-
-        //public void PlayExecute()
-        //{
-        //    var startMenu = DataContext as StartMenuViewModel;
-        //    startMenu.StartNewGame(categoryBox.SelectedIndex);
-        //}
-
-        //public bool CanPlayExecute()
-        //{
-        //    return true;
-        //}
     }
 }
